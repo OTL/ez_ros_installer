b3bb38b13a2e07e9f33a990d8fc98b52f1bc7b4e
Takashi Ogura <t.ogura@gmail.com>
2017-08-15 13:31:10 +0900

    Modify apt-key

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ if [[ "${yes_no}" == "q" ]]; then
 fi
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get install ros-"${DISTRO}"-"${WHAT}" python-rosinstall
 sudo rosdep init
